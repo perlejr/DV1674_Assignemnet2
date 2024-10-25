168ee99d376ae1ef6247b48260a1b580e1b660e0
perle <perleswejr@hotmail.se>
2024-10-25 15:58:27 +0200

    Verify updated with easier way to see if blur is correct

--- blur/verify.sh
+++ blur/verify.sh
@@ -21,6 +21,7 @@ do
             echo "${red}Error: Incongruent output data detected when blurring image $image.ppm with $thread thread(s)${reset}"
             status=1
         fi
+        echo "Image ${image} blured with ${thread} thread(s) passed the test successfully"
 
         rm "./data_o/blur_${image}_par.ppm"
     done
